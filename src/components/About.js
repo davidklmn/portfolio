5c891144c366ce2621372b5cd81451a27fbab8f7
davidklmn <davidkelemen88@gmail.com>
2023-08-02 18:50:21 +0100

    About section with scroll animation

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,55 @@
+import { useEffect } from "react";
+
+import "./about.css";
+
+import SplitType from "split-type";
+import Lenis from "@studio-freight/lenis";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 export default function About() {
-  return <h1>About</h1>;
+  gsap.registerPlugin(ScrollTrigger);
+
+  //* Basic Lenis Smooth Scroll
+  const lenis = new Lenis();
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
+  //* Split Type which splits the p tag into divs
+
+  useEffect(() => {
+    function runSplit() {
+      const text = new SplitType("#split", { types: "words, chars" });
+
+      gsap.from(text.chars, {
+        scrollTrigger: {
+          trigger: text.chars,
+          start: "top 90%",
+          end: "top 40%",
+          scrub: true,
+          markers: false,
+        },
+        opacity: 0.2,
+        stagger: 0.1,
+      });
+    }
+
+    runSplit();
+  });
+
+  return (
+    <div id="about" className="about-container">
+      <div>
+        <p id="split">
+          I am an enthisuastic learner and thinker. I love to learn new things
+          and jump right into problemsolving. And I loooooove pizza!
+        </p>
+      </div>
+    </div>
+  );
 }
