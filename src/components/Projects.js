3bc2dcc6dc241547a47d47ed635a46c28481026f
davidklmn <davidkelemen88@gmail.com>
2023-08-08 11:01:57 +0100

    Projects touchup

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,9 +18,45 @@ export default function Projects() {
             ></img>
           </div>
         </div>
-        <div id="card2" className="cards"></div>
-        <div id="card3" className="cards"></div>
-        <div id="card4" className="cards"></div>
+        <div id="card2" className="cards small-card">
+          <div className="card-text">
+            <p>Project</p>
+            <h2>Portfolio</h2>
+          </div>
+          <div className="card-image">
+            <img
+              className="proj-img"
+              src="/images/portfolio_site.png"
+              alt="portfolio"
+            ></img>
+          </div>
+        </div>
+        <div id="card3" className="cards small-card">
+          <div className="card-text">
+            <p>Project</p>
+            <h2>Portfolio</h2>
+          </div>
+          <div className="card-image">
+            <img
+              className="proj-img"
+              src="/images/portfolio_site.png"
+              alt="portfolio"
+            ></img>
+          </div>
+        </div>
+        <div id="card4" className="cards">
+          <div className="card-text">
+            <p>Project</p>
+            <h2>Portfolio</h2>
+          </div>
+          <div className="card-image">
+            <img
+              className="proj-img"
+              src="/images/portfolio_site.png"
+              alt="portfolio"
+            ></img>
+          </div>
+        </div>
       </div>
     </div>
   );
