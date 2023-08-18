74fcc75b3b6e20c79b5472e7a09f18639c7a19ae
davidklmn <davidkelemen88@gmail.com>
2023-08-18 13:12:26 +0100

    Home blob mousemove added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,18 @@ import "./home.css";
 import { ReactComponent as Vector } from "../pictures/blobanimation.svg";
 
 export default function Home() {
+  const onMouseMove = (e) => {
+    e.preventDefault();
+
+    var valueX = (e.clientX * -1) / 10;
+    var valueY = (e.clientY * -1) / 10;
+    let blob = document.querySelector(".blob");
+
+    blob.style.transform = `translate3d(${valueX}px, ${valueY}px,0)`;
+  };
+
+  window.addEventListener("mousemove", onMouseMove);
+
   return (
     <div id="home" className="main-container">
       <Vector className="blob" />
