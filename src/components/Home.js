6d95aa54c919207e3a00ae8fd1bb2fa7eb28bb47
davidklmn <davidkelemen88@gmail.com>
2023-08-22 19:55:45 +0100

    Fixed mobile screen issues

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import "./home.css";
 import { ReactComponent as Vector } from "../pictures/blobanimation.svg";
 
 export default function Home() {
-  const onMouseMove = (e) => {
+  function mouseMove(e) {
     e.preventDefault();
 
     var valueX = (e.clientX * -1) / 10;
@@ -10,12 +10,10 @@ export default function Home() {
     let blob = document.querySelector(".blob");
 
     blob.style.transform = `translate3d(${valueX}px, ${valueY}px,0)`;
-  };
-
-  window.addEventListener("mousemove", onMouseMove);
+  }
 
   return (
-    <div id="home" className="main-container">
+    <div onMouseMove={mouseMove} id="home" className="main-container">
       <Vector className="blob" />
       <div className="itsme">
         <h2>Hello, I'm</h2>
