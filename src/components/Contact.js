6f43e4ff676e75ae8ea10ce65ffc5dad2137f1e7
davidklmn <davidkelemen88@gmail.com>
2023-08-23 17:44:34 +0100

    Contact form useState added for dynamic value tracking

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./contact.css";
 import { ReactComponent as Vector } from "../pictures/blobanimation.svg";
 
@@ -7,24 +7,32 @@ import emailjs from "@emailjs/browser";
 export default function Contact() {
   const form = useRef();
 
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_mfw3zxk",
-        "template_dd6g67l",
-        form.current,
-        "ETWB3HYBoOPQFvkZo"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    // emailjs
+    //   .sendForm(
+    //     "service_mfw3zxk",
+    //     "template_dd6g67l",
+    //     form.current,
+    //     "ETWB3HYBoOPQFvkZo"
+    //   )
+    //   .then(
+    //     (result) => {
+    //       console.log(result.text);
+    //     },
+    //     (error) => {
+    //       console.log(error.text);
+    //     }
+    //   );
+
+    setUserName("");
+    setUserEmail("");
+    setMessage("");
   };
 
   return (
@@ -33,9 +41,26 @@ export default function Contact() {
       <h1>Contact.</h1>
       <div className="form-container">
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="user_name" placeholder="name" />
-          <input type="text" name="user_email" placeholder="email" />
-          <textarea name="message" placeholder="message"></textarea>
+          <input
+            type="text"
+            name="user_name"
+            placeholder="name"
+            value={userName}
+            onChange={(e) => setUserName(e.currentTarget.value)}
+          />
+          <input
+            type="text"
+            name="user_email"
+            placeholder="email"
+            value={userEmail}
+            onChange={(e) => setUserEmail(e.currentTarget.value)}
+          />
+          <textarea
+            name="message"
+            placeholder="message"
+            value={message}
+            onChange={(e) => setMessage(e.currentTarget.value)}
+          ></textarea>
           <input type="submit" id="submit" />
         </form>
       </div>
