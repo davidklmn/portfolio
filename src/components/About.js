dc904e2f2386db86b34ab0a73496ce4841f5155a
davidklmn <davidkelemen88@gmail.com>
2023-08-29 19:17:34 +0100

    Added about modal

--- src/components/About.js
+++ src/components/About.js
@@ -36,14 +36,52 @@ export default function About() {
     runSplit();
   });
 
+  const modalOverlay = document.querySelector(".modal-overlay");
+  const modal = document.getElementById("about-modal");
+
+  function aboutMe() {
+    // modal.classList.add("active");
+    // modalOverlay.classList.add("active");
+    console.log("heylo");
+  }
+
+  function closeAboutMe() {}
+
   return (
     <div id="about" className="about-container">
       <div>
         <p id="split">
           I am an enthusiastic Front-End Developer who loves responsive
-          user-friendly design... and I looooove pizaaaaaa.
+          user-friendly design... and I looooove pizaaaaaa...
+          <span className="about-me" onClick={aboutMe}>
+            Read more
+          </span>
         </p>
       </div>
+
+      <div className="about-modal" id="about-modal">
+        <button data-modal-close className="modal-close" onClick={closeAboutMe}>
+          &times;
+        </button>
+        <h2 className="about-modal-body">
+          Praesent eu ultrices ligula. Class aptent taciti sociosqu ad litora
+          torquent per conubia nostra, per inceptos himenaeos. Etiam malesuada
+          at risus sit amet porta. Phasellus ornare eget ante vitae dignissim.
+          Praesent eu nisi sodales, consectetur arcu vel, dignissim ex. Donec
+          rhoncus molestie sollicitudin. Nulla facilisis, lacus ut rhoncus
+          tincidunt, nunc dolor pulvinar magna, venenatis ultricies dui diam a
+          eros. Suspendisse sit amet interdum magna. Donec vulputate, magna non
+          convallis porttitor, dui sapien viverra lectus, sagittis congue libero
+          elit faucibus nisl. Etiam sodales, mi eu convallis congue, enim sapien
+          gravida arcu, at dictum quam orci quis sem. Morbi a lorem at metus
+          lacinia scelerisque at eget dolor. Nam eros orci, malesuada tincidunt
+          feugiat in, laoreet ut lorem. Duis eu mauris ipsum. Fusce ullamcorper
+          magna vel finibus venenatis. Cras arcu augue, porttitor eu dignissim
+          non, semper non tellus. Duis vulputate magna ultricies diam finibus,
+          non vulputate tellus egestas.
+        </h2>
+      </div>
+      <div className="modal-overlay"></div>
     </div>
   );
 }
